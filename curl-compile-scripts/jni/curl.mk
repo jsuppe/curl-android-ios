70c941fc065095658d75bdb653df82911ca8325f
build <build@hunter-linux-1.vidyo.com>
2016-11-21 16:41:28 -0500

    Replaced missing files to curl.mk

--- curl-compile-scripts/jni/curl.mk
+++ curl-compile-scripts/jni/curl.mk
@@ -12,17 +12,22 @@ CURL_COMMON_CFLAGS += \
   -Wno-system-headers -Wno-typedef-redefinition -Wno-unused-variable \
   -Wno-unused-function
 CURL_CSOURCES := \
-amigaos.c           curl_fnmatch.c      dict.c           gopher.c     http_chunks.c     ldap.c       nwos.c       share.c         strdup.c        warnless.c \
-asyn-ares.c         curl_gethostname.c  dotdot.c         hash.c       http_digest.c     llist.c      openldap.c   slist.c         strerror.c      wildcard.c \
-asyn-thread.c       curl_gssapi.c       easy.c           hmac.c       http_negotiate.c  md4.c        parsedate.c  smb.c           strtok.c        x509asn1.c \
-base64.c            curl_memrchr.c      escape.c         hostasyn.c   http_ntlm.c       md5.c        pingpong.c   smtp.c          strtoofft.c \
-conncache.c         curl_multibyte.c    file.c           hostcheck.c  http_proxy.c      memdebug.c   pipeline.c   socks.c         system_win32.c \
-connect.c           curl_ntlm_core.c    fileinfo.c       hostip4.c    idn_win32.c       mprintf.c    pop3.c       socks_gssapi.c  telnet.c \
-content_encoding.c  curl_ntlm_wb.c      formdata.c       hostip6.c    if2ip.c           multi.c      progress.c   socks_sspi.c    tftp.c \
-cookie.c            curl_rtmp.c         ftp.c            hostip.c     imap.c            netrc.c      rtsp.c       speedcheck.c    timeval.c \
-curl_addrinfo.c     curl_sasl.c         ftplistparser.c  hostsyn.c    inet_ntop.c       non-ascii.c  security.c   splay.c         transfer.c \
-curl_des.c          curl_sspi.c         getenv.c         http2.c      inet_pton.c       nonblock.c   select.c     ssh.c           url.c \
-curl_endian.c       curl_threads.c      getinfo.c        http.c       krb5.c            nwlib.c      sendf.c      strcase.c       version.c
+amigaos.c	    curl_fnmatch.c	dict.c		 gopher.c     http_chunks.c	ldap.c	     nwos.c	  sendf.c	  strdup.c	  version.c \
+asyn-ares.c	    curl_gethostname.c	dotdot.c	 hash.c       http_digest.c	llist.c      openldap.c   share.c	  strequal.c	  warnless.c \
+asyn-thread.c	    curl_gssapi.c	easy.c		 hmac.c       http_negotiate.c	md4.c	     parsedate.c  slist.c	  strerror.c	  wildcard.c \
+base64.c	    curl_memrchr.c	escape.c	 hostasyn.c   http_ntlm.c	md5.c	     pingpong.c   smb.c		  strtok.c	  x509asn1.c \
+conncache.c	    curl_multibyte.c	file.c		 hostcheck.c  http_proxy.c	memdebug.c   pipeline.c   smtp.c	  strtoofft.c \
+connect.c	    curl_ntlm_core.c	fileinfo.c	 hostip4.c    idn_win32.c	mprintf.c    pop3.c	  socks.c	  system_win32.c \
+content_encoding.c  curl_ntlm_wb.c	formdata.c	 hostip6.c    if2ip.c		multi.c      progress.c   socks_gssapi.c  telnet.c \
+cookie.c	    curl_rtmp.c		ftp.c		 hostip.c     imap.c		netrc.c      rawstr.c	  socks_sspi.c	  tftp.c \
+curl_addrinfo.c     curl_sasl.c		ftplistparser.c  hostsyn.c    inet_ntop.c	non-ascii.c  rtsp.c	  speedcheck.c	  timeval.c \
+curl_des.c	    curl_sspi.c		getenv.c	 http2.c      inet_pton.c	nonblock.c   security.c   splay.c	  transfer.c \
+curl_endian.c	    curl_threads.c	getinfo.c	 http.c       krb5.c		nwlib.c      select.c	  ssh.c		  url.c \
+vauth/cleartext.c  vauth/digest.c	vauth/krb5_gssapi.c  vauth/ntlm.c	vauth/oauth2.c	       vauth/spnego_sspi.c \
+vauth/cram.c	   vauth/digest_sspi.c	vauth/krb5_sspi.c    vauth/ntlm_sspi.c	vauth/spnego_gssapi.c  vauth/vauth.c \
+vtls/axtls.c   vtls/darwinssl.c  vtls/gtls.c	 vtls/nss.c	 vtls/polarssl.c	     vtls/schannel.c \
+vtls/cyassl.c  vtls/gskit.c	 vtls/mbedtls.c  vtls/openssl.c  vtls/polarssl_threadlock.c  vtls/vtls.c
+
 CURL_LOCAL_SRC_FILES := $(addprefix ../../curl/lib/,$(CURL_CSOURCES))
 CURL_LOCAL_C_INCLUDES += \
   $(LOCAL_PATH)/../../curl/include \
